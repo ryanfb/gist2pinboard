f8aa2b025baabe4d470a4c05c6783bf624d61508
Ryan Baumann <ryan.baumann@gmail.com>
2014-07-21 11:52:31 -0400

    Working version, just use rest-client

--- README.md
+++ README.md
@@ -5,5 +5,4 @@ gist2pinboard
 
     ---
     github_token: exampl3498ed4e83a947a1b4e0d76490186fc4
-    pinboard_username: example
-    pinboard_password: ex4mpl3
+    pinboard_token: example:ABCDEF0ABCDEF1ABCDEF2
--- gist2pinboard.rb
+++ gist2pinboard.rb
@@ -1,11 +1,40 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
-require 'ghee'
-require 'pinboard'
+require 'rest_client'
+require 'json'
+
+def fetch_gist_page(gist_page, github_token)
+  $stderr.puts "Fetching page #{gist_page}"
+  response = RestClient.get("https://api.github.com/gists", :params => { :access_token => github_token, :page => gist_page }, :accept => :json)
+  return gists = JSON.parse(response)
+end
+
+def fetch_all_gists(github_token)
+  all_gists = []
+  response = RestClient.get("https://api.github.com/gists", :params => { :access_token => github_token }, :accept => :json)
+  gists = JSON.parse(response)
+  pages = response.headers[:link].scan(/(\d+)>; rel="last"$/).first.first.to_i
+  puts "Pages: #{pages}"
+  all_gists += gists
+  (2..pages).each do |page|
+    all_gists += fetch_gist_page(page, github_token)
+  end
+  return all_gists
+end
+
+def get_title(gist)
+  if gist['description'].nil? || gist['description'].empty?
+    return "gist:#{gist['id']}"
+  else
+    return gist['description']
+  end
+end
 
 config = YAML.load_file('.secrets.yml')
 
-gh = Ghee.access_token(config['github_token'])
-
-$stderr.puts gh.gists.inspect
+gists = fetch_all_gists(config['github_token'])
+gists.each do |gist|
+  $stderr.puts "Adding: " + gist['html_url']
+  RestClient.get("https://api.pinboard.in/v1/posts/add", :params => { :auth_token => config['pinboard_token'], :url => gist['html_url'], :description => get_title(gist), :shared => 'no', :tags => 'gist2pinboard' }, :accept => :json)
+end
